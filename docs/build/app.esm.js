edd9b30285fc5dd63f05b495210eefffc714d2c2
Shingo Tanikawa <dubai2k@gmail.com>
2021-02-03 19:14:55 +0900

    docs

--- docs/build/app.esm.js
+++
@@ -1,1 +0,0 @@
-import{p,b as a}from"./p-9f302888.js";(()=>{const a=import.meta.url,r={};return""!==a&&(r.resourcesUrl=new URL(".",a).href),p(r)})().then((p=>a([["p-cb9a12b4",[[1,"app-game"],[1,"app-board"],[1,"app-info"],[1,"app-square",{index:[2],label:[1]}]]]],p)));
