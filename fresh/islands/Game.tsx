350b225a659e0b28b5400df73575cd54e5232989
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-06-26 15:45:09 +0900

    state

--- fresh/islands/Game.tsx
+++ fresh/islands/Game.tsx
@@ -4,7 +4,15 @@ import { h, Component } from 'preact';
 import Board from '../components/Board.tsx';
 import { MoveEvent } from '../components/Square.tsx';
 
+import { Squares, calculateWinner } from '../utils/game.ts';
+
 export default class Game extends Component {
+  state = {
+    history: [Array(9).fill(null) as Squares],
+    step: 0,
+    xIsNext: true
+  }
+
   componentDidMount() {
     console.log(this.base);
     this.base?.addEventListener('move', this.handleMove);
@@ -16,12 +24,31 @@ export default class Game extends Component {
 
   handleMove = (e: Event) => {
     console.log((e as MoveEvent).detail);
+    const i = (e as MoveEvent).detail.index;
+
+    const state = this.state;
+
+    const h = state.history.slice(0, state.step + 1);
+    const current = h[h.length - 1];
+    const squares = current.slice();
+    if (calculateWinner(squares) || squares[i]) return;
+
+    squares[i] = state.xIsNext ? 'X' : 'O';
+
+    this.setState({
+      history: [...h, squares],
+      step: h.length,
+      xIsNext: !state.xIsNext
+    });
   }
 
   render() {
     console.log('Game');
 
-    const squares = ['', '', '', '', '', '', '', '', ''];
-    return <Board squares={squares} />
+    const state = this.state;
+    const current = state.history[state.step];
+    const winner = calculateWinner(current);
+
+    return <Board squares={current} />
   }
 }
